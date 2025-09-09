21501f1e4f6d27cb437f3fa2cf7aca89225f2830
ccn <ccn@cuppajoeman.com>
2025-09-09 17:41:03 -0400

    feat: update logger and demo new class parser

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,24 @@ int main() {
 
     cpp_parsing::test();
 
-    if (true) {
+    auto class_str = "class X { public: int val = 1; bool thing = false; bool thing2; };";
+
+    // cpp_parsing::test_parser(class_str, cpp_parsing::class_def_parser_good);
+    auto pr = cpp_parsing::class_def_parser_good->parse(class_str, 0);
+    auto parser_name_to_matches = cpp_parsing::collect_matches_by_parser_name(pr);
+
+    // std::cout << "gooblas" << std::endl;
+    // std::cout << pr.to_string() << std::endl;
+
+    // I don't think ti works because something about the recurssion stopping
+    for (const auto &parser_name : {cpp_parsing::assignment_parser->name, cpp_parsing::declaration_parser->name}) {
+        std::cout << parser_name << std::endl;
+        for (const auto &match : parser_name_to_matches[parser_name]) {
+            std::cout << match << std::endl;
+        }
+    }
+
+    if (false) {
         auto s = "basic_math.cpp";
 
         // auto tlfs = cpp_parsing::extract_top_level_functions(s);
